8a066bd74745db7c1676c9934b16d95dddca03e9
BLawson <ducdeswin@outlook.com>
2017-07-06 00:48:30 +0200

    WIP

--- android-app/app/src/main/java/com/echopen/asso/echopen/MainActivity.java
+++ android-app/app/src/main/java/com/echopen/asso/echopen/MainActivity.java
@@ -40,30 +40,14 @@ import com.echopen.asso.echopen.utils.Timer;
  * These two methods should be refactored into one
  */
 
-public class MainActivity extends CustomActivity implements AbstractActionActivity, EchographyImageVisualisationContract.View {
+public class MainActivity extends Activity {
 
-    /* integer constant that switch whether the photo or the video is on */
-    private int display;
-    /* class that deals with the view of MainActivity */
-    private MainActionController mainActionController;
-    public GestureDetector gesture;
-    /* Ruler enables get the overview of body scales */
-    public RulerView rulerView;
-    /* Setter for rulerView */
-    public void setRulerView(RulerView rulerView) {
-        this.rulerView = rulerView;
-    }
     /* constant setting the process via local, UDP or TCP */
     public static boolean LOCAL_ACQUISITION = false;
 
     public static boolean TCP_ACQUISITION = true;
 
     public static boolean UDP_ACQUISITION = false;
-
-    private EchographyImageStreamingService mEchographyImageStreamingService;
-    private RenderingContextController mRenderingContextController;
-
-    private EchographyImageVisualisationContract.Presenter mEchographyImageVisualisationPresenter;
 
     /**
      * This method calls all the UI methods and then gives hand to  UDPToBitmapDisplayer class.
@@ -75,96 +59,7 @@ public class MainActivity extends CustomActivity implements AbstractActionActivi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /* loading config constants in singleton Class */
-        Config.getInstance(this);
-        Config.singletonConfig.getWidth();
-        /* aiming to synchronize views : when operator drags lines to measure distance between points,
-         the measure is synchronized with a TextView */
-        Synchronizer.getInstance(this);
-
         setContentView(R.layout.activity_main);
 
-        mEchographyImageStreamingService = ((EchOpenApplication) this.getApplication() ).getEchographyImageStreamingService();
-        mRenderingContextController = mEchographyImageStreamingService.getRenderingContextController();
-
-        mEchographyImageVisualisationPresenter = new EchographyImageVisualisationPresenter(mEchographyImageStreamingService, this);
-        this.setPresenter(mEchographyImageVisualisationPresenter);
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-        mEchographyImageVisualisationPresenter.start();
-        EchographyImageStreamingTCPMode tcpMode = new EchographyImageStreamingTCPMode(Constants.Http.REDPITAYA_IP, Constants.Http.REDPITAYA_PORT);
-        mEchographyImageStreamingService.connect(tcpMode, this);
-    }
-
-    /*
-    * initActionController() is used to separate concerns
-    * MainActionController sets the main UI elements and handles the display of the main screen picture
-    * @param no params
-    * */
-    public void initActionController() {
-        Activity activity = this;
-        mainActionController = new MainActionController(activity);
-    }
-
-    /**
-     * Handles clickable View that enable to choose camera or to start a new intent
-     *
-     * @param v, the clickable View
-     */
-    @Override
-    public void onClick(View v) {
-        super.onClick(v);
-    }
-
-    /**
-     * @param item, MenuItem instance
-     * @return boolean
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return true;
-    }
-
-    /**
-     * Following the doc https://developer.android.com/intl/ko/training/basics/intents/result.html,
-     * onActivityResult is “Called when an activity you launched exits, giving you the requestCode you started it with,
-     * the resultCode it returned, and any additional data from it.”,
-     * See more here : https://stackoverflow.com/questions/20114485/use-onactivityresult-android
-     *
-     * @param requestCode, integer argument that identifies your request
-     * @param resultCode, to get its values, check RESULT_CANCELED, RESULT_OK here https://developer.android.com/reference/android/app/Activity.html#RESULT_OK
-     * @param data,       Intent instance
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        startActivity(new Intent(this, MainActivity.class));
-    }
-
-    @Override
-    public void refreshImage(final Bitmap iBitmap) {
-        try{
-            this.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    ImageView echoImage = (ImageView) findViewById(R.id.echo);
-                    echoImage.setImageBitmap(iBitmap);
-                    echoImage.setColorFilter(Config.colorMatrixColorFilter);
-                    Timer.logResult("Display Bitmap");
-                }
-            });
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-    }
-
-    @Override
-    public void setPresenter(EchographyImageVisualisationContract.Presenter iPresenter) {
-        mEchographyImageVisualisationPresenter = iPresenter;
     }
 }
